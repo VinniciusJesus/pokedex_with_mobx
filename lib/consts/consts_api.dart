7087495cb2b5bc89380a5378b13475c6b1ca6407
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-08-04 20:33:46 -0300

    criando constantes

--- lib/consts/consts_api.dart
+++ lib/consts/consts_api.dart
@@ -1,3 +1,7 @@
 class ConstsAPI {
   static String pokeapiURL = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+  static String pokeapiv2URL = 'https://pokeapi.co/api/v2/pokemon/';
+  static String pokeapiv2EspeciesURL = 'https://pokeapi.co/api/v2/pokemon-species/';
+
+  static getColorType({String type}) {}
 }
